3851ed1a2b5bc166c2457bfc1be7c3f8838c05ac
ameta <ameta@student.21-school.ru>
2021-08-05 20:37:33 +0300

    client

--- srcs/client.c
+++ srcs/client.c
@@ -2,5 +2,18 @@
 
 int main(int ac, char **av)
 {
+    int pid;
+
+    if (ac != 3)
+    {
+        write(1, "Error\nWrong number of arguments\n", 34);
+        return (0);
+    }
+    pid = ft_atoi(av[1]);
+    if (pid < 0 || pid > 30000)
+    {
+        write(1, "Error\nWrong pid\n", 18);
+        return (0);
+    }
     
 }
